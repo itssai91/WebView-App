406ea8f24a4495e2c8982e422673fbeae6368a7d
Saikrishna Panda <itssai91@gmail.com>
2020-12-20 15:38:43 +0530

    App Complete

---
+++ app/src/main/java/in/hostranger/webviewtestapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package `in`.hostranger.webviewtestapp
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private val webView: WebView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://biologycraze.com")
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+    }
+    override fun onBackPressed() {
+        if (webView!!.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+
+    }
+}
