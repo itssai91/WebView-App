e05f9ef882c6d0cbe9b28dfb32fef3378ae21174
Saikrishna Panda <itssai91@gmail.com>
2020-12-20 19:15:45 +0530

    some files changed

--- app/src/main/java/in/hostranger/webviewtestapp/MainActivity.kt
+++ app/src/main/java/in/hostranger/webviewtestapp/MainActivity.kt
@@ -5,22 +5,29 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.JavascriptInterface
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private val webView: WebView? = null
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val webView = findViewById<WebView>(R.id.webView)
         webView.webViewClient = WebViewClient()
-        webView.loadUrl("https://biologycraze.com")
+        webView.loadUrl("https://quantomweb.com")
         val webSettings = webView.settings
         webSettings.javaScriptEnabled = true
+        webSettings.pluginState = WebSettings.PluginState.ON
+        webSettings.allowFileAccess = true
+        webSettings.allowContentAccess = true
+        webSettings.setEnableSmoothTransition(true)
     }
     override fun onBackPressed() {
         if (webView!!.canGoBack()) {
